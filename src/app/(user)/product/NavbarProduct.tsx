fda2e3dd1b2a446ba3d174fb9491c8d24333405b
Digzcod <stephenjaramillo5@gmail.com>
2023-05-30 22:49:25 +0800

    primary

--- src/app/(user)/product/NavbarProduct.tsx
+++ src/app/(user)/product/NavbarProduct.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const NavbarProduct = () => {
   return (
     <>
-      <AppBar color='warning'>
+      <AppBar color='primary'>
         <Toolbar>
             <Divider>
                 <Typography>Product Navbar</Typography>
